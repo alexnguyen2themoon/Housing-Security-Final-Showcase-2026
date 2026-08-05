8467963bfe7f8f49d8489684178678c9f7ab2971
alexnguyen2themoon <alexnguyen.adastra@gmail.com>
2026-08-05 11:43:28 -0700

    fixed MapScreen route

--- src/navigation/UserTab.js
+++ src/navigation/UserTab.js
@@ -5,7 +5,7 @@ import { View, StyleSheet, Pressable, Text, Button } from "react-native";
 import { supabase } from "../../utils/hooks/supabase";
 // Import the Supabase client
 // Screens
-import MapScreen from "../screens/testerScreen";
+import MapScreen from "../screens/MapScreen";
 import CameraScreen from "../screens/CameraScreen";
 import StoriesScreen from "../screens/StoriesScreen";
 import SpotlightScreen from "../screens/SpotlightScreen";
